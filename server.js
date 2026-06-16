ed179dafe7b5e6f3e0b6a59847302f36a20cb7e6
Aleksandar Sredojevic <aleksandarsredojevic2@gmail.com>
2026-06-16 22:50:47 +0200

    e

--- server.js
+++ server.js
@@ -69,4 +69,5 @@ wss.on('connection', (ws) => {
     });
 });
 
-server.listen(3000, () => console.log('Server radi na portu 3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server radi na portu ${PORT}`));
